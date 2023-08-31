0432e7fe9c5415b970309f3f7c4ae7a7a277c814
Pablo <cardosopab@gmail.com>
2023-08-30 23:17:35 -0500

    + modal w/ EditTicketController

--- src/controllers/TicketsController.tsx
+++ src/controllers/TicketsController.tsx
@@ -1,13 +1,20 @@
 import { useSelector } from "react-redux";
 import TicketsView from "../views/tickets/TicketsView"
 import { RootState } from "../models/redux/store";
+import { useState } from "react";
 
 const TicketsController = () => {
     const tickets = useSelector((state: RootState) => state.tickets.value);
     const users = useSelector((state: RootState) => state.users.value);
     const projects = useSelector((state: RootState) => state.projects.value);
+    const [open, setOpen] = useState(false);
+
+    const handleModal = () => {
+        setOpen(!open);
+    }
+    
     return (
-        <TicketsView tickets={tickets} users={users} projects={projects} />
+        <TicketsView tickets={tickets} users={users} projects={projects} open={open} handleModal={handleModal} />
     )
 }
 export default TicketsController
--- src/views/tickets/TicketsView.tsx
+++ src/views/tickets/TicketsView.tsx
@@ -1,16 +1,35 @@
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Box, Button, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import DrawerComponent from "../DrawerComponent"
 import Ticket from "../../models/Ticket";
 import User from "../../models/User";
 import Project from "../../models/Project";
+import { useSelector } from "react-redux";
+import { RootState } from "../../models/redux/store";
+import EditTicketController from "../../controllers/EditTicketController";
 
 interface TicketsProps {
   tickets: Ticket[];
   users: User[];
   projects: Project[];
+  handleModal: any;
+  open: boolean;
 }
 function Tickets(props: TicketsProps) {
-  const { tickets, users, projects } = props;
+  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+  const { tickets, users, projects, handleModal, open } = props;
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    minWidth: 400,
+    bgcolor: 'background.paper',
+    border: '3px solid #1976d2',
+    borderRadius: '1em',
+    boxShadow: 24,
+    p: 4,
+  };
   return (
     <>
       <DrawerComponent />
@@ -29,7 +48,8 @@ function Tickets(props: TicketsProps) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {tickets.map(({ id, title, projectId, submitterId, personnelId, priority, status, type, createdAt }) => {
+            {tickets.map(ticket => {
+              const { id, title, projectId, submitterId, personnelId, priority, status, type, createdAt } = ticket
               const submitter = users.find(user => user.id === submitterId);
               const project = projects.find(project => project.id === projectId);
               return (
@@ -43,7 +63,17 @@ function Tickets(props: TicketsProps) {
                   <TableCell>{createdAt}</TableCell>
                   <TableCell>
                     <div className="column">
-                      <Button>Edit</Button>
+                      <Button onClick={handleModal}>Edit</Button>
+                      <Modal
+                        open={open}
+                        onClose={handleModal}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                      >
+                        <Box sx={style}>
+                          <EditTicketController ticket={ticket} />
+                        </Box>
+                      </Modal>
                       <Button>Details</Button>
                     </div>
                   </TableCell>
