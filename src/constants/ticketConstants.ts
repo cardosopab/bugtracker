d0d1bca72bde39d116f3a95e233453afb943810c
Pablo <cardosopab@gmail.com>
2023-09-07 20:26:51 -0500

    Create ticketConstants

---
+++ src/constants/ticketConstants.ts
@@ -0,0 +1,5 @@
+const statusOptions: string[] = ["Backlog", "Today", "Doing", "Blocked", "Done"]
+const priorityOptions: string[] = ['Low', 'Medium', 'High',];
+const typeOptions: string[] = ['Bug/Errors', 'Feature', 'Task', 'Documentation', 'Testing/QA', 'Feedback', 'Duplicate/Invalid'];
+
+export { statusOptions, priorityOptions, typeOptions };
