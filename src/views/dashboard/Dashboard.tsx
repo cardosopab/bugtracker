8050315cbc47043d346add6d0dc5b1e5f56f7a34
Pablo <cardosopab@gmail.com>
2023-09-12 23:32:44 -0500

    Add useEffect to compute graphs

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -5,57 +5,70 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../models/redux/store";
 import { Card, CardHeader } from "@mui/material";
 import { priorityOptions, statusOptions, typeOptions } from "../../constants/ticketConstants";
+import { useEffect, useState } from "react";
+import Ticket from "../../models/Ticket";
 
 function Dashboard() {
+    const barChartParams = { margin: { bottom: 100, left: 40, right: 20 } };
     const tickets = useSelector((state: RootState) => state.tickets.value);
     const users = useSelector((state: RootState) => state.users.value);
+    const [priorityCount, setPriorityCount] = useState<number[]>(new Array(priorityOptions.length).fill(0));
+    const [statusCount, setStatusCount] = useState<number[]>(new Array(statusOptions.length).fill(0));
+    const [topTypeCount, setTopTypeCount] = useState<{ id: number; value: number; label: string; }[]>([]);
+    const [topPersonnelCount, setTopPersonnelCount] = useState<{ id: number; value: number; label: string; }[]>([]);
 
-    const priorityCount: number[] = new Array(priorityOptions.length).fill(0);
-    const statusCount: number[] = new Array(statusOptions.length).fill(0);
-    const typeCount: { id: number, value: number; label: string }[] = [];
-    const personnelCount: { id: number, value: number; label: string }[] = [];
-    tickets.forEach(ticket => {
-        const priorityIdx = priorityOptions.indexOf(ticket.priority);
-        if (priorityIdx !== -1) {
-            priorityCount[priorityIdx] += 1;
-        }
+    useEffect(() => {
+        const updatedPriorityCount = new Array(priorityOptions.length).fill(0);
+        const updatedStatusCount = new Array(statusOptions.length).fill(0);
+        const updatedTypeCount: { id: number, value: number; label: string }[] = [];
+        const updatedPersonnelCount: { id: number, value: number; label: string }[] = [];
 
-        const statusIdx = statusOptions.indexOf(ticket.status);
-        if (statusIdx !== -1) {
-            statusCount[statusIdx] += 1;
-        }
-
-        const typeIdx = typeOptions.indexOf(ticket.type);
-        if (typeIdx !== -1) {
-            if (!typeCount[typeIdx]) {
-                typeCount[typeIdx] = { id: typeIdx, value: 0, label: typeOptions[typeIdx] };
+        tickets.forEach((ticket: Ticket) => {
+            const priorityIdx = priorityOptions.indexOf(ticket.priority);
+            if (priorityIdx !== -1) {
+                updatedPriorityCount[priorityIdx] += 1;
             }
-            typeCount[typeIdx].value += 1;
-        }
 
-        const personnel = users.find(user => user.id === ticket.personnelId);
-        if (personnel != undefined) {
+            const statusIdx = statusOptions.indexOf(ticket.status);
+            if (statusIdx !== -1) {
+                updatedStatusCount[statusIdx] += 1;
+            }
 
-            const personnelIdx = users.indexOf(personnel!);
-            if (personnelIdx !== -1) {
-                if (!personnelCount[personnelIdx]) {
-                    personnelCount[personnelIdx] = { id: personnelIdx, value: 0, label: personnel.name };
+            const typeIdx = typeOptions.indexOf(ticket.type);
+            if (typeIdx !== -1) {
+                if (!updatedTypeCount[typeIdx]) {
+                    updatedTypeCount[typeIdx] = { id: typeIdx, value: 0, label: typeOptions[typeIdx] };
                 }
-                personnelCount[personnelIdx].value += 1;
+                updatedTypeCount[typeIdx].value += 1;
             }
-        }
-        console.log('assignmentCount', personnelCount)
-    });
 
-    typeCount.sort((a, b) => b.value - a.value);
-    personnelCount.sort((a, b) => b.value - a.value);
+            const personnel = users.find((user) => user.id === ticket.personnelId);
+            if (personnel) {
+                const personnelIdx = users.indexOf(personnel);
+                if (personnelIdx !== -1) {
+                    if (!updatedPersonnelCount[personnelIdx]) {
+                        updatedPersonnelCount[personnelIdx] = { id: personnelIdx, value: 0, label: personnel.name };
+                    }
+                    updatedPersonnelCount[personnelIdx].value += 1;
+                }
+            }
+        });
 
-    // Slice the arrays to keep only the top 4 objects
-    const topTypeCount = typeCount.slice(0, 4);
-    const topPersonnelCount = personnelCount.slice(0, 4);
-    const barChartParams = {
-        margin: { bottom: 100, left: 40, right: 20 },
-    };
+        const updatedTopTypeCount = [...updatedTypeCount];
+        const updatedTopPersonnelCount = [...updatedPersonnelCount];
+
+        // Sort the arrays to keep only the top 4 objects
+        updatedTopTypeCount.sort((a, b) => b.value - a.value);
+        updatedTopPersonnelCount.sort((a, b) => b.value - a.value);
+
+        // Update the state variables
+        setPriorityCount(updatedPriorityCount);
+        setStatusCount(updatedStatusCount);
+        setTopTypeCount(updatedTopTypeCount);
+        setTopPersonnelCount(updatedTopPersonnelCount);
+    }, [tickets, users]);
+
+
     return (
         <>
             <DrawerController>
