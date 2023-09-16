3c358df53d3c932d7d618c55023db0c5aa4fbd16
Pablo <cardosopab@gmail.com>
2023-09-16 08:57:58 -0500

    Remove navigation to USERS_URL

--- src/controllers/ProjectsController.tsx
+++ src/controllers/ProjectsController.tsx
@@ -7,35 +7,41 @@ import ProjectsView from "../views/projects/ProjectsView";
 import { useNavigate } from "react-router-dom";
 import { setDrawerIndex } from "../models/redux/drawerSlice";
 import { setProjectDetails } from "../models/redux/projectDetailsSlice";
-import { PROJECT_DETAILS_URL, USERS_URL } from "../views/viewsUrls";
+import { PROJECT_DETAILS_URL } from "../views/viewsUrls";
 
 function ProjectsController() {
-    const navigateTo = useNavigate();
-    const { handleSubmit, register, formState: { errors } } = useForm();
-    const dispatch = useDispatch();
-    const projects = useSelector((state: RootState) => state.projects.value);
+  const navigateTo = useNavigate();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const dispatch = useDispatch();
+  const projects = useSelector((state: RootState) => state.projects.value);
 
-    const onSubmit = (values: any) => {
-        console.log(values.name, values.description)
-        createProject(values.name, values.description);
-        values.name = ''
-        values.description = '';
-    };
+  const onSubmit = (values: any) => {
+    console.log(values.name, values.description);
+    createProject(values.name, values.description);
+    values.name = "";
+    values.description = "";
+  };
 
-    function navigateToUsers() {
-        navigateTo(USERS_URL)
-        dispatch(setDrawerIndex(2));
-    }
-
-    function navigateToDetails(details: Project) {
-        navigateTo(PROJECT_DETAILS_URL)
-        dispatch(setDrawerIndex(90));
-        dispatch(setProjectDetails(details))
-    }
-    return (
-        <>
-            <ProjectsView handleSubmit={handleSubmit} onSubmit={onSubmit} register={register} projects={projects} errors={errors} navigateToUsers={navigateToUsers} navigateToDetails={navigateToDetails} />
-        </>
-    )
+  function navigateToDetails(details: Project) {
+    navigateTo(PROJECT_DETAILS_URL);
+    dispatch(setDrawerIndex(90));
+    dispatch(setProjectDetails(details));
+  }
+  return (
+    <>
+      <ProjectsView
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        register={register}
+        projects={projects}
+        errors={errors}
+        navigateToDetails={navigateToDetails}
+      />
+    </>
+  );
 }
-export default ProjectsController
+export default ProjectsController;
