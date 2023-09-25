4366f88d99e9f0eab64679dba647c4c371aa8d0d
Pablo <cardosopab@gmail.com>
2023-09-25 16:45:18 -0500

    Add TicketEditModalView

--- src/controllers/components/TicketEditModalController.tsx
+++ src/controllers/components/TicketEditModalController.tsx
@@ -1,28 +1,9 @@
-import {
-  Button,
-  Card,
-  CardHeader,
-  FormControl,
-  InputLabel,
-  Input,
-  MenuItem,
-  Select,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  Modal,
-} from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../../models/redux/store";
 import { useState } from "react";
 import Ticket from "../../models/Ticket";
-import {
-  priorityOptions,
-  statusOptions,
-  typeOptions,
-} from "../../constants/ticketConstants";
 import { useTicketActions } from "../../models/database/hooks/useTicketActions";
+import TicketEditModalView from "../../views/components/ticket_edit_modal/TicketEditModalView";
 
 interface EditTicketProps {
   ticket: Ticket;
@@ -37,12 +18,13 @@ const TicketEditModalController = ({ ticket, title }: EditTicketProps) => {
   const [titleValue, setTitleValue] = useState(ticket.title);
   const [descriptionValue, setDescriptionValue] = useState(ticket.description);
   const [selectedProject, setSelectedProject] = useState(
-    projects.find((project) => project.id === ticket.projectId)?.name
+    projects.find((project) => project.id === ticket.projectId)?.name ??
+      projects[0].name
   );
   const [selectedPriority, setSelectedPriority] = useState(ticket.priority);
   const [selectedType, setSelectedType] = useState(ticket.type);
   const [selectedPersonnel, setSelectedPersonnel] = useState(
-    users.find((user) => user.id === ticket.personnelId)?.name
+    users.find((user) => user.id === ticket.personnelId)?.name ?? users[0].name
   );
   const [selectedStatus, setSelectedStatus] = useState(ticket.status);
 
@@ -58,197 +40,31 @@ const TicketEditModalController = ({ ticket, title }: EditTicketProps) => {
     deleteTicket(ticketId);
   };
 
-  const style = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    minWidth: 400,
-  };
-
   return (
-    <>
-      <Button
-        fullWidth
-        style={{ justifyContent: "flex-start" }}
-        onClick={() => handleModalToggle()}
-      >
-        {title ? title : "Edit"}
-      </Button>
-      <Modal open={isModalOpen} onClose={() => handleModalToggle()}>
-        <Card style={style}>
-          <CardHeader title={`Edit Ticket: ${ticket.title}`} />
-          <Table style={{ tableLayout: "fixed" }}>
-            <TableBody>
-              <TableRow>
-                <TableCell style={{ width: "50%", verticalAlign: "top" }}>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel htmlFor="title-input">Title</InputLabel>
-                    <Input
-                      id="title-input"
-                      value={titleValue}
-                      onChange={(event) => setTitleValue(event.target.value)}
-                    />
-                  </FormControl>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel id="project-dropdown-label">Project</InputLabel>
-                    <Select
-                      labelId="project-dropdown-label"
-                      label="Project"
-                      id="project-dropdown"
-                      value={selectedProject} // Connect to state
-                      onChange={(event) =>
-                        setSelectedProject(event.target.value)
-                      } // Update state on change
-                    >
-                      {projects.map((project) => (
-                        <MenuItem key={project.id} value={project.name}>
-                          {project.name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel id="priority-dropdown-label">
-                      Ticket Priority
-                    </InputLabel>
-                    <Select
-                      labelId="priority-dropdown-label"
-                      label="Ticket Priority"
-                      id="priority-dropdown"
-                      value={selectedPriority} // Connect to state
-                      onChange={(event) =>
-                        setSelectedPriority(event.target.value)
-                      } // Update state on change
-                    >
-                      {priorityOptions.map((option) => (
-                        <MenuItem key={option} value={option}>
-                          {option}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel id="type-dropdown-label">
-                      Ticket Type
-                    </InputLabel>
-                    <Select
-                      labelId="type-dropdown-label"
-                      label="Ticket Type"
-                      id="type-dropdown"
-                      value={selectedType} // Connect to state
-                      onChange={(event) => setSelectedType(event.target.value)} // Update state on change
-                    >
-                      {typeOptions.map((option) => (
-                        <MenuItem key={option} value={option}>
-                          {option}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </TableCell>
-                <TableCell style={{ width: "50%", verticalAlign: "top" }}>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel htmlFor="description-input">
-                      Description
-                    </InputLabel>
-                    <Input
-                      id="description-input"
-                      value={descriptionValue}
-                      onChange={(event) =>
-                        setDescriptionValue(event.target.value)
-                      }
-                      multiline
-                      rows={4}
-                    />
-                  </FormControl>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel id="developer-dropdown-label">
-                      Personnel
-                    </InputLabel>
-                    <Select
-                      labelId="developer-dropdown-label"
-                      label="Personnel"
-                      id="developer-dropdown"
-                      value={selectedPersonnel} // Connect to state
-                      onChange={(event) =>
-                        setSelectedPersonnel(event.target.value)
-                      } // Update state on change
-                    >
-                      {users.map((user) => (
-                        <MenuItem key={user.id} value={user.name}>
-                          {user.name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                  <FormControl fullWidth={true} margin={"normal"}>
-                    <InputLabel id="status-dropdown-label">Status</InputLabel>
-                    <Select
-                      fullWidth={true}
-                      labelId="status-dropdown-label"
-                      label="Status"
-                      id="status-dropdown"
-                      value={selectedStatus} // Connect to state
-                      onChange={(event) =>
-                        setSelectedStatus(event.target.value)
-                      } // Update state on change
-                    >
-                      {statusOptions.map((option) => (
-                        <MenuItem key={option} value={option}>
-                          {option}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell colSpan={2}>
-                  <div
-                    style={{ display: "flex", justifyContent: "space-between" }}
-                  >
-                    <Button
-                      variant="contained"
-                      color="error"
-                      onClick={() => handleTicketRemoval(ticket.id)}
-                    >
-                      Delete Ticket
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() =>
-                        handleUpdate(ticket.id, {
-                          id: ticket.id,
-                          projectId: projects.find(
-                            (project) => project.name === selectedProject
-                          )!.id,
-                          companyId: ticket.companyId,
-                          title: titleValue,
-                          description: descriptionValue,
-                          submitterId: ticket.submitterId,
-                          personnelId: users.find(
-                            (user) => user.name === selectedPersonnel
-                          )!.id,
-                          priority: selectedPriority,
-                          status: selectedStatus,
-                          type: selectedType,
-                          createdAt: ticket.createdAt,
-                          comments: ticket.comments,
-                        })
-                      }
-                    >
-                      Update Ticket
-                    </Button>
-                  </div>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </Card>
-      </Modal>
-    </>
+    <TicketEditModalView
+      users={users}
+      projects={projects}
+      title={title}
+      isModalOpen={isModalOpen}
+      ticket={ticket}
+      titleValue={titleValue}
+      descriptionValue={descriptionValue}
+      selectedPersonnel={selectedPersonnel}
+      selectedPriority={selectedPriority}
+      selectedProject={selectedProject}
+      selectedStatus={selectedStatus}
+      selectedType={selectedType}
+      setTitleValue={setTitleValue}
+      setDescriptionValue={setDescriptionValue}
+      setSelectedPersonnel={setSelectedPersonnel}
+      setSelectedProject={setSelectedProject}
+      setSelectedPriority={setSelectedPriority}
+      setSelectedStatus={setSelectedStatus}
+      setSelectedType={setSelectedType}
+      handleUpdate={handleUpdate}
+      handleModalToggle={handleModalToggle}
+      handleTicketRemoval={handleTicketRemoval}
+    />
   );
 };
 
---
+++ src/views/components/ticket_edit_modal/TicketEditModalView.tsx
@@ -0,0 +1,267 @@
+import {
+  Button,
+  Card,
+  CardHeader,
+  FormControl,
+  InputLabel,
+  Input,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Modal,
+} from "@mui/material";
+import User from "../../../models/User";
+import Project from "../../../models/Project";
+import Ticket from "../../../models/Ticket";
+import {
+  priorityOptions,
+  statusOptions,
+  typeOptions,
+} from "../../../constants/ticketConstants";
+
+interface TicketEditModalViewProps {
+  users: User[];
+  projects: Project[];
+  ticket: Ticket;
+  title?: string;
+  titleValue: string;
+  isModalOpen: boolean;
+  descriptionValue: string;
+  selectedPersonnel: string;
+  selectedPriority: string;
+  selectedProject: string;
+  selectedStatus: string;
+  selectedType: string;
+  setTitleValue: React.Dispatch<React.SetStateAction<string>>;
+  setDescriptionValue: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedPersonnel: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedPriority: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedProject: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedStatus: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedType: React.Dispatch<React.SetStateAction<string>>;
+  handleUpdate: (ticketId: string, ticket: Ticket) => void;
+  handleTicketRemoval: (ticketId: string) => void;
+  handleModalToggle: () => void;
+}
+
+const TicketEditModalView = ({
+  users,
+  projects,
+  title,
+  isModalOpen,
+  ticket,
+  titleValue,
+  descriptionValue,
+  selectedPersonnel,
+  selectedPriority,
+  selectedProject,
+  selectedStatus,
+  selectedType,
+  setTitleValue,
+  setDescriptionValue,
+  setSelectedPersonnel,
+  setSelectedProject,
+  setSelectedPriority,
+  setSelectedStatus,
+  setSelectedType,
+  handleUpdate,
+  handleModalToggle,
+  handleTicketRemoval,
+}: TicketEditModalViewProps) => {
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: 400,
+  };
+
+  return (
+    <>
+      <Button
+        fullWidth
+        style={{ justifyContent: "flex-start" }}
+        onClick={() => handleModalToggle()}
+      >
+        {title ? title : "Edit"}
+      </Button>
+      <Modal open={isModalOpen} onClose={() => handleModalToggle()}>
+        <Card style={style}>
+          <CardHeader title={`Edit Ticket: ${ticket.title}`} />
+          <Table style={{ tableLayout: "fixed" }}>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ width: "50%", verticalAlign: "top" }}>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel htmlFor="title-input">Title</InputLabel>
+                    <Input
+                      id="title-input"
+                      value={titleValue}
+                      onChange={(event) => setTitleValue(event.target.value)}
+                    />
+                  </FormControl>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel id="project-dropdown-label">Project</InputLabel>
+                    <Select
+                      labelId="project-dropdown-label"
+                      label="Project"
+                      id="project-dropdown"
+                      value={selectedProject}
+                      onChange={(event) =>
+                        setSelectedProject(event.target.value)
+                      }
+                    >
+                      {projects.map((project) => (
+                        <MenuItem key={project.id} value={project.name}>
+                          {project.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel id="priority-dropdown-label">
+                      Ticket Priority
+                    </InputLabel>
+                    <Select
+                      labelId="priority-dropdown-label"
+                      label="Ticket Priority"
+                      id="priority-dropdown"
+                      value={selectedPriority}
+                      onChange={(event) =>
+                        setSelectedPriority(event.target.value)
+                      }
+                    >
+                      {priorityOptions.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel id="type-dropdown-label">
+                      Ticket Type
+                    </InputLabel>
+                    <Select
+                      labelId="type-dropdown-label"
+                      label="Ticket Type"
+                      id="type-dropdown"
+                      value={selectedType}
+                      onChange={(event) => setSelectedType(event.target.value)}
+                    >
+                      {typeOptions.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </TableCell>
+                <TableCell style={{ width: "50%", verticalAlign: "top" }}>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel htmlFor="description-input">
+                      Description
+                    </InputLabel>
+                    <Input
+                      id="description-input"
+                      value={descriptionValue}
+                      onChange={(event) =>
+                        setDescriptionValue(event.target.value)
+                      }
+                      multiline
+                      rows={4}
+                    />
+                  </FormControl>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel id="developer-dropdown-label">
+                      Personnel
+                    </InputLabel>
+                    <Select
+                      labelId="developer-dropdown-label"
+                      label="Personnel"
+                      id="developer-dropdown"
+                      value={selectedPersonnel}
+                      onChange={(event) =>
+                        setSelectedPersonnel(event.target.value)
+                      }
+                    >
+                      {users.map((user) => (
+                        <MenuItem key={user.id} value={user.name}>
+                          {user.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormControl fullWidth={true} margin={"normal"}>
+                    <InputLabel id="status-dropdown-label">Status</InputLabel>
+                    <Select
+                      fullWidth={true}
+                      labelId="status-dropdown-label"
+                      label="Status"
+                      id="status-dropdown"
+                      value={selectedStatus}
+                      onChange={(event) =>
+                        setSelectedStatus(event.target.value)
+                      }
+                    >
+                      {statusOptions.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2}>
+                  <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Button
+                      variant="contained"
+                      color="error"
+                      onClick={() => handleTicketRemoval(ticket.id)}
+                    >
+                      Delete Ticket
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() =>
+                        handleUpdate(ticket.id, {
+                          id: ticket.id,
+                          projectId: projects.find(
+                            (project) => project.name === selectedProject
+                          )!.id,
+                          companyId: ticket.companyId,
+                          title: titleValue,
+                          description: descriptionValue,
+                          submitterId: ticket.submitterId,
+                          personnelId: users.find(
+                            (user) => user.name === selectedPersonnel
+                          )!.id,
+                          priority: selectedPriority,
+                          status: selectedStatus,
+                          type: selectedType,
+                          createdAt: ticket.createdAt,
+                          comments: ticket.comments,
+                        })
+                      }
+                    >
+                      Update Ticket
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+      </Modal>
+    </>
+  );
+};
+export default TicketEditModalView;
