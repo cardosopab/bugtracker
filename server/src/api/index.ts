5da68b9408b79b659bb5595ba4f813598abeb7ce
Pablo <cardosopab@gmail.com>
2024-01-15 21:45:53 -0600

    Add /users router

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,16 +1,18 @@
-import express from 'express';
+import express from "express";
 
-import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
+import MessageResponse from "../interfaces/MessageResponse";
+import emojis from "./emojis";
+import users from "./users";
 
 const router = express.Router();
 
-router.get<{}, MessageResponse>('/', (req, res) => {
+router.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏',
+    message: "API - 👋🌎🌍🌏",
   });
 });
 
-router.use('/emojis', emojis);
+router.use("/emojis", emojis);
+router.use("/users", users);
 
 export default router;
