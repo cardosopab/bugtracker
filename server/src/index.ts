c96472c4ec25106a9c88f72b2aa1cfa22f0bd615
Pablo <cardosopab@gmail.com>
2024-01-18 02:10:24 -0600

    Add connectDatabase()

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
-import app from './app';
+import app from "./app";
+import connectDatabase from "./connectDatabase";
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
+  connectDatabase();
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
