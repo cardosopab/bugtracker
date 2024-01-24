387262d82e87a818fa96a031074bacfd311cb891
Pablo <cardosopab@gmail.com>
2024-01-23 21:14:43 -0600

    Add companyRouter

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth";
 import usersRouter from "./users";
+import companyRouter from "./company";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(usersRouter);
+router.use(companyRouter);
 
 export default router;
