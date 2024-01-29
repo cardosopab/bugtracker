766b9df4626e2f5fe4825672bc4d66ae354ecdc8
Pablo <cardosopab@gmail.com>
2024-01-28 23:28:37 -0600

    Add endpoints

---
+++ server/src/contants/endpoints.ts
@@ -0,0 +1,9 @@
+export const AuthEndpoints = {
+  LOGIN: "/api/auth/login",
+  STATUS: "/api/auth/status",
+  LOGOUT: "/api/auth/logout",
+};
+
+export const UsersEndpoints = {
+  USERS: "/api/users",
+};
