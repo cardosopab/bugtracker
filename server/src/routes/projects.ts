04be439df286376480a484dbba0e923f0f29b3d5
Pablo <cardosopab@gmail.com>
2024-01-29 01:39:12 -0600

    Add by name & by id routes

--- server/src/routes/projects.ts
+++ server/src/routes/projects.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import "../strategies/local-strategies";
 import { checkSchema } from "express-validator";
-import { ProjectValidationSchema } from "../utils/validationSchemas/projectValidationSchema";
-import { createProjectHandler } from "../handlers/projectHandlers";
+import {
+  ProjectValidationSchema,
+  ProjectIdValidationSchema,
+  ProjectNameValidationSchema,
+} from "../utils/validationSchemas/projectValidationSchema";
+import {
+  createProjectHandler,
+  deleteProjectHandler,
+  readProjectByNameHandler,
+} from "../handlers/projectHandlers";
 import { ProjectsEndpoints } from "../constants/endpoints";
 
 const router = Router();
@@ -12,6 +20,20 @@ router.post(
   ProjectsEndpoints.PROJECTS,
   checkSchema(ProjectValidationSchema),
   createProjectHandler
+);
+
+// Read By Name
+router.get(
+  ProjectsEndpoints.PROJECT_BY_NAME,
+  checkSchema(ProjectNameValidationSchema),
+  readProjectByNameHandler
+);
+
+// Delete
+router.delete(
+  ProjectsEndpoints.PROJECT_BY_ID,
+  checkSchema(ProjectIdValidationSchema),
+  deleteProjectHandler
 );
 
 export default router;
