f96f3a238a399b563d676336671ac677da79379b
Pablo <cardosopab@gmail.com>
2024-01-29 01:31:57 -0600

    Add read by name & id, and delete by id

--- server/src/handlers/projectHandlers.ts
+++ server/src/handlers/projectHandlers.ts
@@ -15,3 +15,52 @@ export const createProjectHandler = async (req: Request, res: Response) => {
     return res.sendStatus(400);
   }
 };
+
+export const readProjectByNameHandler = async (req: Request, res: Response) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) return res.status(400).send(result.array());
+  const data = matchedData(req);
+  try {
+    const project = await Project.findOne(data);
+    return res.status(200).send(project);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
+
+export const readProjectByIdHandler = async (req: Request, res: Response) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) return res.status(400).send(result.array());
+  const data = matchedData(req);
+  try {
+    const project = await Project.findById(data);
+    return res.status(200).send(project);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
+
+export const addUserToProjectHandler = async (
+  req: Request,
+  res: Response
+) => {};
+
+export const deleteUserFromProjectHandler = async (
+  req: Request,
+  res: Response
+) => {};
+
+export const deleteProjectHandler = async (req: Request, res: Response) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) return res.status(400).send(result.array());
+  const data = matchedData(req);
+  try {
+    Project.findByIdAndDelete(data.projectId);
+    return res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
