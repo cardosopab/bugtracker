3bd1ba334ca0635b75234911bbb39d83e50e8955
Pablo <cardosopab@gmail.com>
2024-01-31 05:01:48 -0600

    Add read by email handler

--- server/src/handlers/userHandlers.ts
+++ server/src/handlers/userHandlers.ts
@@ -17,3 +17,34 @@ export const createUserHandler = async (req: Request, res: Response) => {
     return res.sendStatus(400);
   }
 };
+
+export const readUserByEmailHandler = async (req: Request, res: Response) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) return res.status(400).send(result.array());
+  const data = matchedData(req);
+  try {
+    const user = await User.findOne(data);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    return res.status(200).send(user);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
+
+export const deleteUserByIdHandler = async (req: Request, res: Response) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) return res.status(400).send(result.array());
+  const data = matchedData(req);
+  try {
+    await User.findByIdAndDelete(data.userdId);
+    return res.sendStatus(204);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
+};
