78de0a110dcb0207e8f25afacd8b8a85655fee28
Pablo <cardosopab@gmail.com>
2024-02-03 04:53:25 -0600

    Add testing for 2 user creates & admin checks

--- server/test/users.test.ts
+++ server/test/users.test.ts
@@ -12,91 +12,33 @@ describe("User life cycle", () => {
   const MONGO_TEST_URI = process.env.MONGO_TEST_URI!;
   let app: any;
   let loginResult: any;
-  let userMongoId: mongoose.Types.ObjectId;
+  let firstMongoUserId: mongoose.Types.ObjectId;
   let secondMongoUserId: mongoose.Types.ObjectId;
   const dateNow = Date.now();
-  const name = `test_${dateNow}`;
-  const email = `${name}@email.com`;
+  const firstName = `test_${dateNow}`;
+  const firstEmail = `${firstName}@email.com`;
   const secondName = `test_${dateNow + 1}`;
   const secondEmail = `${dateNow + 1}@email.com`;
   const password = "test1234";
   const companyId = new mongoose.Types.ObjectId();
   const updatedCompanyId = new mongoose.Types.ObjectId();
+  const adminRole = "Admin";
+  const developerRole = "Developer";
 
   beforeAll(async () => {
     app = createApp();
     await connectDatabase(MONGO_TEST_URI);
   });
 
-  test("should create user", async () => {
+  test("should create first user", async () => {
     const res = await request(app).post(UsersEndpoints.USERS).send({
-      name: name,
+      name: firstName,
       password: password,
-      email: email,
+      email: firstEmail,
       companyId: companyId,
-      role: "Admin",
+      role: adminRole,
     });
     expect(res.statusCode).toBe(201);
-  });
-
-  test("should get all users", async () => {
-    const res = await request(app).get(UsersEndpoints.USERS);
-    const filteredUser = res.body.filter((user: SerializedUser) => {
-      return user.name === name;
-    });
-    expect(res.statusCode).toBe(200);
-    expect(filteredUser[0].name).toBe(name);
-  });
-
-  test("should log user in", async () => {
-    const res = await request(app).post(AuthEndpoints.LOGIN).send({
-      email: email,
-      password: password,
-    });
-    loginResult = res;
-    console.log(`loginResult: ${loginResult}`);
-    expect(res.statusCode).toBe(200);
-  });
-
-  test("should find user by email", async () => {
-    const res = await request(app)
-      .get(UsersEndpoints.USER_BY_EMAIL)
-      .set("Cookie", loginResult.headers["set-cookie"])
-      .send({
-        email: email,
-      });
-    expect(res.statusCode).toBe(200);
-    expect(res.body.name).toBe(name);
-    userMongoId = res.body._id;
-  });
-
-  test("should find user by id", async () => {
-    const res = await request(app)
-      .get(UsersEndpoints.USER_BY_ID)
-      .set("Cookie", loginResult.headers["set-cookie"])
-      .send({
-        userId: userMongoId,
-      });
-    expect(res.statusCode).toBe(200);
-    expect(res.body.name).toBe(name);
-    expect(JSON.stringify(res.body.companyId)).toEqual(
-      JSON.stringify(companyId)
-    );
-  });
-
-  test("should update user by id", async () => {
-    const res = await request(app)
-      .patch(UsersEndpoints.USER_BY_ID)
-      .set("Cookie", loginResult.headers["set-cookie"])
-      .send({
-        userId: userMongoId,
-        companyId: updatedCompanyId,
-      });
-    expect(res.statusCode).toBe(200);
-    expect(res.body.name).toBe(name);
-    expect(JSON.stringify(res.body.companyId)).toEqual(
-      JSON.stringify(updatedCompanyId)
-    );
   });
 
   test("should create second user", async () => {
@@ -105,12 +47,34 @@ describe("User life cycle", () => {
       password: password,
       email: secondEmail,
       companyId: companyId,
-      role: "Developer",
+      role: developerRole,
     });
     expect(res.statusCode).toBe(201);
   });
 
-  test("should find second user by email", async () => {
+  test("should login first user", async () => {
+    const res = await request(app).post(AuthEndpoints.LOGIN).send({
+      email: firstEmail,
+      password: password,
+    });
+    loginResult = res;
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("should find first user by email as admin", async () => {
+    const res = await request(app)
+      .get(UsersEndpoints.USER_BY_EMAIL)
+      .set("Cookie", loginResult.headers["set-cookie"])
+      .send({
+        email: firstEmail,
+      });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.name).toBe(firstName);
+    expect(res.body.role).toBe(adminRole);
+    firstMongoUserId = res.body._id;
+  });
+
+  test("should find second user by email as admin", async () => {
     const res = await request(app)
       .get(UsersEndpoints.USER_BY_EMAIL)
       .set("Cookie", loginResult.headers["set-cookie"])
@@ -119,7 +83,91 @@ describe("User life cycle", () => {
       });
     expect(res.statusCode).toBe(200);
     expect(res.body.name).toBe(secondName);
+    expect(res.body.role).toBe(developerRole);
     secondMongoUserId = res.body._id;
+  });
+
+  test("should find second user by id as admin", async () => {
+    const res = await request(app)
+      .get(UsersEndpoints.USER_BY_ID)
+      .set("Cookie", loginResult.headers["set-cookie"])
+      .send({
+        userId: secondMongoUserId,
+      });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.name).toBe(secondName);
+    expect(JSON.stringify(res.body.companyId)).toEqual(
+      JSON.stringify(companyId)
+    );
+  });
+
+  test("should update second user by id as admin", async () => {
+    const res = await request(app)
+      .patch(UsersEndpoints.USER_BY_ID)
+      .set("Cookie", loginResult.headers["set-cookie"])
+      .send({
+        userId: secondMongoUserId,
+        companyId: updatedCompanyId,
+      });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.name).toBe(secondName);
+    expect(JSON.stringify(res.body.companyId)).toEqual(
+      JSON.stringify(updatedCompanyId)
+    );
+  });
+
+  test("should logout first user", async () => {
+    const res = await request(app)
+      .post(AuthEndpoints.LOGOUT)
+      .set("Cookie", loginResult.headers["set-cookie"]);
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("should login second user", async () => {
+    const res = await request(app).post(AuthEndpoints.LOGIN).send({
+      email: secondEmail,
+      password: password,
+    });
+    loginResult = res;
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("should fail to find first user by email", async () => {
+    const res = await request(app)
+      .get(UsersEndpoints.USER_BY_EMAIL)
+      .set("Cookie", loginResult.headers["set-cookie"])
+      .send({
+        email: firstEmail,
+      });
+    expect(res.statusCode).toBe(403);
+  });
+
+  test("should get all users without being admin", async () => {
+    const res = await request(app).get(UsersEndpoints.USERS);
+    const filteredUser: SerializedUser[] = res.body.filter(
+      (user: SerializedUser) => {
+        return user.name === firstName;
+      }
+    );
+    expect(res.statusCode).toBe(200);
+    expect(filteredUser[0].name).toBe(firstName);
+    expect(res.body.length).toBe(2);
+  });
+
+  test("should logout second user", async () => {
+    const res = await request(app)
+      .post(AuthEndpoints.LOGOUT)
+      .set("Cookie", loginResult.headers["set-cookie"]);
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("should login first user", async () => {
+    const res = await request(app).post(AuthEndpoints.LOGIN).send({
+      email: firstEmail,
+      password: password,
+    });
+    loginResult = res;
+    expect(res.statusCode).toBe(200);
   });
 
   test("should delete second user", async () => {
