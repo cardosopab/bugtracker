9726f4b4beba3a836b2f61d0303f836823980631
Pablo <cardosopab@gmail.com>
2024-02-03 05:45:53 -0600

    Add find by name & by id, should add & delete personnel, should delete company tests

--- server/test/companies.test.ts
+++ server/test/companies.test.ts
@@ -10,10 +10,13 @@ dotenv.config();
 describe("Company life cycle", () => {
   const MONGO_TEST_URI = process.env.MONGO_TEST_URI!;
   let app: any;
-  let loginResult: any;
+  let mongoCompanyId: any;
   const dateNow = Date.now();
   const name = `test_${dateNow}`;
-  const personnelArray = ["Steve Wozniak", "Steve Jobs"];
+  const firstPersonnel = new mongoose.Types.ObjectId();
+  const secondPersonnel = new mongoose.Types.ObjectId();
+  const personnelIdArray = [firstPersonnel];
+  const updatedPersonnelIdArrary = [...personnelIdArray, secondPersonnel];
 
   beforeAll(async () => {
     app = createApp();
@@ -23,9 +26,68 @@ describe("Company life cycle", () => {
   test("should create company", async () => {
     const res = await request(app).post(CompaniesEndpoints.COMPANIES).send({
       name: name,
-      personnel: personnelArray,
+      personnel: personnelIdArray,
     });
     expect(res.statusCode).toBe(201);
+  });
+
+  test("should find company by name", async () => {
+    const res = await request(app)
+      .get(CompaniesEndpoints.COMPANY_BY_NAME)
+      .send({
+        name: name,
+      });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.name).toBe(name);
+    mongoCompanyId = res.body._id;
+  });
+
+  test("should find company by id", async () => {
+    const res = await request(app).get(CompaniesEndpoints.COMPANY_BY_ID).send({
+      companyId: mongoCompanyId,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.name).toBe(name);
+    expect(res.body._id).toBe(mongoCompanyId);
+  });
+
+  test("should add personnel id", async () => {
+    const res = await request(app).post(CompaniesEndpoints.PERSONNEL).send({
+      companyId: mongoCompanyId,
+      personnelId: secondPersonnel,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.name).toBe(name);
+    expect(res.body._id).toBe(mongoCompanyId);
+    expect(res.body.personnel.map(String).sort()).toEqual(
+      updatedPersonnelIdArrary.map(String).sort()
+    );
+  });
+
+  test("should delete personnel id", async () => {
+    const res = await request(app).delete(CompaniesEndpoints.PERSONNEL).send({
+      companyId: mongoCompanyId,
+      personnelId: secondPersonnel,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.name).toBe(name);
+    expect(res.body._id).toBe(mongoCompanyId);
+    expect(res.body.personnel.map(String).sort()).toEqual(
+      personnelIdArray.map(String).sort()
+    );
+  });
+
+  test("should delete company by id", async () => {
+    const res = await request(app)
+      .delete(CompaniesEndpoints.COMPANY_BY_ID)
+      .send({
+        companyId: mongoCompanyId,
+      });
+    expect(res.statusCode).toBe(204);
   });
 
   afterAll(async () => {
