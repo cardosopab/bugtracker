ffb7cdde74722992ad4f863dbf17e512d7308de0
Pablo <cardosopab@gmail.com>
2024-02-03 05:42:53 -0600

    Add company name, company id, and personnel validation

--- server/src/utils/validationSchemas/companyValidationSchema.ts
+++ server/src/utils/validationSchemas/companyValidationSchema.ts
@@ -9,3 +9,44 @@ export const CompanyValidationSchema = {
   },
   personnel: {},
 };
+
+export const CompanyNameValidationSchema = {
+  name: {
+    notEmpty: {
+      errorMessage: "name cannot be empty",
+    },
+    isString: {
+      errorMessage: "name is not a string",
+    },
+  },
+};
+
+export const CompanyIdValidationSchema = {
+  companyId: {
+    notEmpty: {
+      errorMessage: "company id cannot be empty",
+    },
+    isString: {
+      errorMessage: "company id is not a string",
+    },
+  },
+};
+
+export const PersonnelValidationSchema = {
+  companyId: {
+    notEmpty: {
+      errorMessage: "company id cannot be empty",
+    },
+    isString: {
+      errorMessage: "company id is not a string",
+    },
+  },
+  personnelId: {
+    notEmpty: {
+      errorMessage: "personnel id cannot be empty",
+    },
+    isString: {
+      errorMessage: "personnel id is not a string",
+    },
+  },
+};
