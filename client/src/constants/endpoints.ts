a3bb836307341d39bd73bc33828eaa13d0b11a01
Pablo <cardosopab@gmail.com>
2024-02-03 18:34:04 -0600

    Add enpoints

---
+++ client/src/constants/endpoints.ts
@@ -0,0 +1,26 @@
+export const AuthEndpoints = {
+    LOGIN: "/api/auth/login",
+    STATUS: "/api/auth/status",
+    LOGOUT: "/api/auth/logout",
+  };
+  
+  export const UsersEndpoints = {
+    USERS: "/api/users",
+    USER_BY_EMAIL: "/api/users/by-email",
+    USER_BY_ID: "/api/users/by-id",
+  };
+  
+  export const ProjectsEndpoints = {
+    PROJECTS: "/api/projects",
+    PROJECT_BY_NAME: "/api/projects/by-name",
+    PROJECT_BY_ID: "/api/projects/by-id",
+    PERSONNEL: "/api/projects/personnel",
+  };
+  
+  export const CompaniesEndpoints = {
+    COMPANIES: "/api/companies",
+    COMPANY_BY_NAME: "/api/companies/by-name",
+    COMPANY_BY_ID: "/api/companies/by-id",
+    PERSONNEL: "/api/companies/personnel",
+  };
+  
