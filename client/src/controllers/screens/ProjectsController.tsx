1e022c15c72be3a4b1f654031930baca3397f2c2
Pablo <cardosopab@gmail.com>
2024-02-05 19:26:02 -0600

    Add readProjects

--- client/src/controllers/screens/ProjectsController.tsx
+++ client/src/controllers/screens/ProjectsController.tsx
@@ -8,9 +8,10 @@ import { setDrawerIndex } from "../../models/redux/drawerSlice";
 import { setProjectDetails } from "../../models/redux/projectDetailsSlice";
 import { PROJECT_DETAILS_URL } from "../../constants/screensUrls";
 import { useProjectActions } from "../../models/database/hooks/useProjectActions";
+import { useEffect } from "react";
 
 const ProjectsController = () => {
-  const createProject = useProjectActions().createProject;
+  const { createProject, readProjects } = useProjectActions();
   const navigateTo = useNavigate();
   const {
     handleSubmit,
@@ -31,6 +32,11 @@ const ProjectsController = () => {
     dispatch(setDrawerIndex(90));
     dispatch(setProjectDetails(details));
   };
+
+  useEffect(() => {
+    readProjects();
+  }, []);
+
   return (
     <>
       <ProjectsView
