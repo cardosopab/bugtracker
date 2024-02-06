c0955ba1d226f563743b2533b88cfb074f577941
Pablo <cardosopab@gmail.com>
2024-02-05 19:23:54 -0600

    Add dev & prod .env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,4 +19,8 @@ export default defineConfig({
       },
     },
   },
+  define: {
+    'process.env.VITE_DEV_MODE': process.env.NODE_ENV !== 'production',
+    'process.env.VITE_API_BASE_URL': process.env.VITE_API_BASE_URL,
+  },
 });
