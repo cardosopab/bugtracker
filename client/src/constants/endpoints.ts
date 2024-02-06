8b0d45196d1edacaa2e540b553ee3f6a59550be1
Pablo <cardosopab@gmail.com>
2024-02-05 19:24:08 -0600

    Add base url

--- client/src/constants/endpoints.ts
+++ client/src/constants/endpoints.ts
@@ -1,26 +1,26 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const AuthEndpoints = {
-    LOGIN: "/api/auth/login",
-    STATUS: "/api/auth/status",
-    LOGOUT: "/api/auth/logout",
-  };
-  
-  export const UsersEndpoints = {
-    USERS: "/api/users",
-    USER_BY_EMAIL: "/api/users/by-email",
-    USER_BY_ID: "/api/users/by-id",
-  };
-  
-  export const ProjectsEndpoints = {
-    PROJECTS: "/api/projects",
-    PROJECT_BY_NAME: "/api/projects/by-name",
-    PROJECT_BY_ID: "/api/projects/by-id",
-    PERSONNEL: "/api/projects/personnel",
-  };
-  
-  export const CompaniesEndpoints = {
-    COMPANIES: "/api/companies",
-    COMPANY_BY_NAME: "/api/companies/by-name",
-    COMPANY_BY_ID: "/api/companies/by-id",
-    PERSONNEL: "/api/companies/personnel",
-  };
-  
+  LOGIN: `${BASE_URL}/api/auth/login`,
+  STATUS: `${BASE_URL}/api/auth/status`,
+  LOGOUT: `${BASE_URL}/api/auth/logout`,
+};
+
+export const UsersEndpoints = {
+  USERS: `${BASE_URL}/api/users`,
+  USER_BY_EMAIL: `${BASE_URL}/api/users/by-email`,
+  USER_BY_ID: `${BASE_URL}/api/users/by-id`,
+};
+
+export const ProjectsEndpoints = {
+  PROJECTS: `${BASE_URL}/api/projects`,
+  PROJECT_BY_NAME: `${BASE_URL}/api/projects/by-name`,
+  PROJECT_BY_ID: `${BASE_URL}/api/projects/by-id`,
+  PERSONNEL: `${BASE_URL}/api/projects/personnel`,
+};
+
+export const CompaniesEndpoints = {
+  COMPANIES: `${BASE_URL}/api/companies`,
+  COMPANY_BY_NAME: `${BASE_URL}/api/companies/by-name`,
+  COMPANY_BY_ID: `${BASE_URL}/api/companies/by-id`,
+  PERSONNEL: `${BASE_URL}/api/companies/personnel`,
+};
