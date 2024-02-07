ffded9eb394c0803808aa4099eab89d224003195
Pablo <cardosopab@gmail.com>
2024-02-07 11:42:50 -0600

    Add tickets router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth";
 import usersRouter from "./users";
 import companiesRouter from "./companies";
 import projectsRouter from "./projects";
+import ticketsRouter from "./tickets";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(authRouter);
 router.use(usersRouter);
 router.use(companiesRouter);
 router.use(projectsRouter);
+router.use(ticketsRouter);
 
 export default router;
