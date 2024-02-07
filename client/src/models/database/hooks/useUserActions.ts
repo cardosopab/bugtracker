aff0d223f4005bd75ba17c186e4ef67c846345df
Pablo <cardosopab@gmail.com>
2024-02-07 12:52:09 -0600

    Update updateUserRole

--- client/src/models/database/hooks/useUserActions.ts
+++ client/src/models/database/hooks/useUserActions.ts
@@ -16,14 +16,18 @@ export const useUserActions = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state: RootState) => state.auth.currentUser);
 
-  const createUser = async (uid: string, name: string, email: string) => {
+  const createUser = async (userId: string, name: string, email: string) => {
+    if (currentUser.role === "Demo") {
+      return;
+    }
+
     try {
-      const docRef = doc(collection(database, USERS_COLLECTION), uid);
+      const docRef = doc(collection(database, USERS_COLLECTION), userId);
       const companyRef = doc(collection(database, COMPANY_COLLECTION));
 
       const date = new Date();
       const newUser: User = {
-        _id: uid,
+        _id: userId,
         createdAt: date,
         name: name,
         email: email,
@@ -34,7 +38,7 @@ export const useUserActions = () => {
         _id: companyRef.id,
         createdAt: date,
         name: "Unassigned",
-        personnel: [uid],
+        personnel: [userId],
       };
       await setDoc(docRef, newUser);
       await setDoc(companyRef, newCompany);
@@ -62,13 +66,29 @@ export const useUserActions = () => {
     }
   };
 
-  const updateUserRole = async (uid: string, role: string) => {
+  const updateUserRole = async (userId: string, role: string) => {
     if (currentUser.role === "Demo") {
       return;
     }
 
-    const user = doc(database, USERS_COLLECTION, uid);
-    setDoc(user, { role: role }, { merge: true });
+    try {
+      const res = await axios.patch(UsersEndpoints.USER_BY_ID, {
+        userId: userId,
+        role: role,
+      });
+      dispatch(setUsers(res.data));
+    } catch (error: any) {
+      if (error.response) {
+        console.error("Server responded with an error:", error.response.status);
+        alert(`Error: ${error.response.data.message}`);
+      } else if (error.request) {
+        console.error("No response received from the server");
+        alert("No response received from the server");
+      } else {
+        console.error("Error setting up the request:", error.message);
+        alert(`Error: ${error.message}`);
+      }
+    }
   };
 
   return { createUser, readUsers, updateUserRole };
