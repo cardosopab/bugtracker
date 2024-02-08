c91ad4b6060c3a588cd1ff86dda0cecf8529ee53
Pablo <cardosopab@gmail.com>
2024-02-07 18:07:18 -0600

    Delete users & tickets read

--- client/src/controllers/screens/TicketsController.tsx
+++ client/src/controllers/screens/TicketsController.tsx
@@ -1,17 +1,13 @@
 import { useSelector } from "react-redux";
 import TicketsView from "../../views/screens/tickets/TicketsView";
 import { RootState } from "../../models/redux/store";
-import { useEffect, useState } from "react";
-import { useUserActions } from "../../models/database/hooks/useUserActions";
-import { useTicketActions } from "../../models/database/hooks/useTicketActions";
+import { useState } from "react";
 
 const TicketsController = () => {
   const tickets = useSelector((state: RootState) => state.tickets.value);
   const users = useSelector((state: RootState) => state.users.value);
   const projects = useSelector((state: RootState) => state.projects.value);
   const [openTickets, setOpenTickets] = useState<{ [id: string]: boolean }>({});
-  const { readUsers } = useUserActions();
-  const { readTickets } = useTicketActions();
 
   const handleModal = (ticketId: string) => {
     setOpenTickets((prevOpenTickets) => ({
@@ -19,11 +15,6 @@ const TicketsController = () => {
       [ticketId]: !prevOpenTickets[ticketId],
     }));
   };
-
-  useEffect(() => {
-    readUsers();
-    readTickets();
-  }, []);
 
   return (
     <TicketsView
