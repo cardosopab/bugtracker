3a05e17b66d353ca2dba73f1216875f375aa3f16
Pablo <cardosopab@gmail.com>
2024-02-19 23:28:40 -0600

    Update to create user & search user

--- client/src/views/screens/users/UsersView.tsx
+++ client/src/views/screens/users/UsersView.tsx
@@ -1,14 +1,6 @@
 import {
-  Button,
   Card,
   CardHeader,
-  FormControl,
-  InputLabel,
-  List,
-  ListItem,
-  ListItemText,
-  MenuItem,
-  Select,
   Table,
   TableBody,
   TableCell,
@@ -18,146 +10,250 @@ import {
   Grid,
   Paper,
   Divider,
+  Input,
+  Button,
+  Select,
+  MenuItem,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  FormControl,
+  Container,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
-import DrawerController from "../../../controllers/components/DrawerController";
+import {
+  FieldValues,
+  RegisterOptions,
+  UseFormHandleSubmit,
+} from "react-hook-form";
+import { Clear, Visibility, VisibilityOff } from "@mui/icons-material";
 import User from "../../../models/User";
-import Project from "../../../models/Project";
 
 interface UsersViewProps {
   users: User[];
-  projects: Project[];
-  selectedUserName: string;
-  selectedProjectName: string;
-  handleProjectDropdown: any;
-  handleUserDropdown: any;
-  handleAddUser: any;
-  handleRemoveUser: any;
-  isRemoveButtonDisabled: boolean;
+  register: (name: string, options?: RegisterOptions) => any;
+  onSubmit: (values: any) => void;
+  handleSubmit: UseFormHandleSubmit<FieldValues, undefined>;
+  errors: any;
+  selectedIsAdmin: string;
+  handleIsAdminDropdown: (event: any) => void;
+
+  showPassword: boolean;
+  handleClickShowPassword: () => void;
+  handleMouseDownPassword: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  searchValue: string;
+  handleSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSearchClear: () => void;
+  filteredUsers: User[] | undefined;
 }
 
 const UsersView = ({
   users,
-  projects,
-  selectedUserName,
-  selectedProjectName,
-  handleUserDropdown,
-  handleProjectDropdown,
-  handleAddUser,
-  handleRemoveUser,
-  isRemoveButtonDisabled,
+  register,
+  onSubmit,
+  handleSubmit,
+  errors,
+  selectedIsAdmin,
+  handleIsAdminDropdown,
+  showPassword,
+  handleClickShowPassword,
+  handleMouseDownPassword,
+  searchValue,
+  handleSearch,
+  handleSearchClear,
+  filteredUsers,
 }: UsersViewProps) => {
+  const theme = useTheme();
+  const isLargeView = useMediaQuery(theme.breakpoints.up("lg")); // Check for lg screen
   return (
-    <DrawerController>
-      {projects.length > 0 ? (
-        <Grid container spacing={2} padding={2}>
-          {/* Title/Header */}
-          <Grid item xs={12}>
-            <h1>Personnel Assignment</h1>
-          </Grid>
+    <>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+        }}
+      >
+        {users.length > 0 ? (
+          <Grid container spacing={2} padding={2}>
+            {/* Title/Header */}
+            <Grid item xs={12}>
+              <h1>User Assignment</h1>
+            </Grid>
 
-          {/* First Column */}
-          <Grid item xs={12} sm={4}>
-            <FormControl margin={"normal"} fullWidth>
-              <InputLabel id="user-dropdown-label">Select a User</InputLabel>
-              <Select
-                labelId="user-dropdown-label"
-                value={selectedUserName}
-                name={selectedUserName}
-                label="Select a User"
-                onChange={handleUserDropdown}
+            {/* First Column */}
+            <Grid item xs={12} sm={4}>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
               >
-                {users.map((option) => (
-                  <MenuItem key={option._id} value={option.name}>
-                    {option.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
-            <FormControl margin={"normal"} fullWidth>
-              <InputLabel id="project-dropdown-label">
-                Select a Project
-              </InputLabel>
-              <Select
-                labelId="project-dropdown-label"
-                value={selectedProjectName}
-                name={selectedProjectName}
-                label="Select a Project"
-                onChange={handleProjectDropdown}
-              >
-                {projects.map((option) => (
-                  <MenuItem key={option._id} value={option.name}>
-                    {option.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <Button
-              onClick={handleAddUser}
-              variant="contained"
-              disabled={!isRemoveButtonDisabled}
-              fullWidth
-              sx={{ marginTop: 2, marginBottom: 2 }}
-            >
-              Add Personnel
-            </Button>
-            <Button
-              onClick={handleRemoveUser}
-              variant="contained"
-              color="error"
-              disabled={isRemoveButtonDisabled}
-              fullWidth
-            >
-              Remove Personnel
-            </Button>
-          </Grid>
-
-          {/* Second Column */}
-          <Grid item xs={12} sm={8}>
-            <Card>
-              <CardHeader title="Projects" subheader="You are a part of:" />
-              <TableContainer component={Paper}>
-                <Table style={{ tableLayout: "fixed" }}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Project Name</TableCell>
-                      <TableCell>Personnel</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {projects.map(({ _id, name, personnel }) => (
-                      <TableRow key={_id}>
-                        <TableCell>{name}</TableCell>
-                        <TableCell>
-                          <div className="column">
-                            <List>
-                              {personnel.map((id) => {
-                                const user = users.find(
-                                  (user) => user._id === id
-                                );
-                                return (
-                                  <ListItem key={id}>
-                                    <ListItemText primary={user?.name ?? ""} />
-                                  </ListItem>
-                                );
-                              })}
-                            </List>
-                          </div>
-                        </TableCell>
-                      </TableRow>
+                <Input
+                  id="name-input"
+                  name="name-input"
+                  sx={{ marginTop: 2, marginBottom: 2 }}
+                  placeholder="Name"
+                  type="text"
+                  {...register("name", { required: "Required" })}
+                />
+                {errors.name && <p>{errors.name.message}</p>}
+                <Input
+                  id="email-input"
+                  name="email-input"
+                  sx={{ marginTop: 2, marginBottom: 2 }}
+                  placeholder="Email"
+                  type="text"
+                  {...register("email", { required: "Required" })}
+                />
+                {errors.email && <p>{errors.email.message}</p>}
+                <Input
+                  id="password-input"
+                  sx={{ marginTop: 2, marginBottom: 2 }}
+                  placeholder="Password"
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  {...register("password", { required: "Required" })}
+                />
+                {errors.password && <p>{errors.password.message}</p>}
+                <FormControl margin="normal" fullWidth>
+                  <InputLabel
+                    // id="is_admin-dropdown-label"
+                    htmlFor="is_admin-dropdown"
+                  >
+                    Is User Admin
+                  </InputLabel>
+                  <Select
+                    id="is_admin-dropdown"
+                    // labelId="is_admin-dropdown-label"
+                    value={selectedIsAdmin}
+                    name={selectedIsAdmin}
+                    label="Is User Admin"
+                    onChange={handleIsAdminDropdown}
+                  >
+                    {["NO", "YES"].map((option) => (
+                      <MenuItem key={option} value={option}>
+                        {option}
+                      </MenuItem>
                     ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Card>
+                  </Select>
+                </FormControl>
+                <Button
+                  variant={"contained"}
+                  type="submit"
+                  fullWidth
+                  sx={{ marginTop: 2 }}
+                >
+                  Create User
+                </Button>
+              </form>
+              <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
+            </Grid>
+
+            {/* Second Column */}
+            <Grid item xs={12} sm={8}>
+              <Card>
+                <CardHeader title="Available Users" subheader="" />
+                <Input
+                  // fullWidth
+                  id="user-filter"
+                  sx={{ margin: 2, marginRight: 2 }}
+                  placeholder="Search by User Name"
+                  type="text"
+                  value={searchValue}
+                  onChange={handleSearch}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="clear search"
+                        onClick={handleSearchClear}
+                        // onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {searchValue.length > 0 && <Clear />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+                <TableContainer component={Paper}>
+                  <Table style={{ tableLayout: "fixed" }}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>User Name</TableCell>
+                        <TableCell>Admin Status</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Actions</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {(filteredUsers?.length ? filteredUsers : users).map(
+                        (user) => {
+                          const { _id, name, email, role } = user;
+                          const isAdmin = role === "Admin";
+                          return (
+                            <TableRow key={_id}>
+                              <TableCell>{name}</TableCell>
+                              <TableCell>
+                                {isAdmin ? "True" : "False"}
+                              </TableCell>
+                              <TableCell
+                                style={
+                                  isLargeView
+                                    ? {}
+                                    : {
+                                        whiteSpace: "normal",
+                                        wordWrap: "break-word",
+                                      }
+                                }
+                              >
+                                {email}
+                              </TableCell>
+                              <TableCell>
+                                {/* <UserEditModalController user={user} /> */}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        }
+                      )}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
-      ) : (
-        <Card className="center">
-          <CardHeader title="No Projects have been created yet!" />
-        </Card>
-      )}
-    </DrawerController>
+        ) : (
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            minHeight="100vh"
+          >
+            <Grid item xs={3}>
+              <Card>
+                <CardHeader title="No users have been created yet." />
+              </Card>
+            </Grid>
+          </Grid>
+        )}
+      </Container>
+    </>
   );
 };
 
