e29e2cd0994e829dcd46cd02e6dbaef5b32d79dd
Pablo <cardosopab@gmail.com>
2024-02-20 21:17:09 -0600

    Add UserEditModalController

---
+++ client/src/controllers/components/UserEditModalController.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import User from "../../models/User";
+import { useUserActions } from "../../models/database/hooks/useUserActions";
+import UserEditModalView from "../../views/components/user_edit_modal/UserEditModalView";
+
+interface EditUserProps {
+  user: User;
+  buttonLabel?: string;
+}
+
+const UserEditModalController = ({ user, buttonLabel }: EditUserProps) => {
+  const { deleteUser, updateUser } = useUserActions();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [nameInput, setNameInput] = useState(user.name);
+  const [isAdmin, setIsAdmin] = useState(user.role == "Admin");
+
+  const handleModalToggle = () => {
+    setIsModalOpen((prev) => !prev);
+    setNameInput(user.name);
+    setIsAdmin(user.role == "Admin");
+  };
+
+  const handleUserRemoval = () => {
+    deleteUser(user._id);
+  };
+
+  const handleNameInputChange = (event: any) => {
+    setNameInput(event.target.value);
+  };
+
+  const handleUserUpdate = () => {
+    const isDiff = user.name != nameInput || (user.role == "Admin") != isAdmin;
+    if (isDiff) {
+      const newUser: User = {
+        _id: user._id,
+        name: nameInput,
+        email: user.email,
+        role: "",
+        companyId: "",
+        createdAt: new Date(),
+        // updateUser(user._id, newUser);
+      };
+      handleModalToggle();
+    }
+  };
+
+  const handleIsAdminDropdown = (event: any) => {
+    const eventValue = event.target.value as string;
+    const eventIsAdmin = eventValue === "YES";
+    setIsAdmin(eventIsAdmin);
+  };
+
+  return (
+    <UserEditModalView
+      user={user}
+      buttonLabel={buttonLabel}
+      nameInput={nameInput}
+      selectedIsAdmin={isAdmin ? "YES" : "NO"}
+      isModalOpen={isModalOpen}
+      handleNameInputChange={handleNameInputChange}
+      handleIsAdminDropdown={handleIsAdminDropdown}
+      handleModalToggle={handleModalToggle}
+      handleUserUpdate={handleUserUpdate}
+      handleUserRemoval={handleUserRemoval}
+    />
+  );
+};
+
+export default UserEditModalController;
