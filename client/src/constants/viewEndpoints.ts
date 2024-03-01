6392f74b6c714e0a696577ddc0ff52aa111cb70a
Pablo <cardosopab@gmail.com>
2024-03-01 01:40:40 -0600

    Rename to view endpoints

--- client/src/constants/viewEndpoints.ts
+++ client/src/constants/viewEndpoints.ts
@@ -1,6 +1,5 @@
 const DASHBOARD_URL = "/dashboard";
 const KANBAN_URL = "/kanban";
-const ROLES_URL = "/roles";
 const USERS_URL = "/users";
 const PROJECTS_URL = "/projects";
 const PROJECT_ASSIGNMENT_URL = "/project-assignment";
@@ -10,7 +9,6 @@ const PROJECT_DETAILS_URL = "/project-details";
 export {
   DASHBOARD_URL,
   KANBAN_URL,
-  ROLES_URL,
   USERS_URL,
   PROJECTS_URL,
   PROJECT_ASSIGNMENT_URL,
