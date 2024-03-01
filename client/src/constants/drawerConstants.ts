50c2b1585c807fd6f5fef2cb1d558f21b79d4680
Pablo <cardosopab@gmail.com>
2024-03-01 01:40:20 -0600

    Fix file extension

--- client/src/constants/drawerConstants.ts
+++ client/src/constants/drawerConstants.ts
@@ -3,21 +3,19 @@ import {
   KANBAN_URL,
   PROJECTS_URL,
   TICKETS_URL,
-  ROLES_URL,
   PROJECT_ASSIGNMENT_URL,
   USERS_URL,
-} from "./screensUrls";
+} from "./viewEndpoints";
 
 const navOptions = [
   { name: "Dashboard", icon: "dashboard", url: DASHBOARD_URL },
   { name: "Kanban Board", icon: "table-chart", url: KANBAN_URL },
   { name: "Projects Overview", icon: "handy-man", url: PROJECTS_URL },
   { name: "Tickets Overview", icon: "list", url: TICKETS_URL },
-  { name: "Role Assignment", icon: "group-add", url: ROLES_URL },
   { name: "Project Assignment", icon: "group", url: PROJECT_ASSIGNMENT_URL },
   { name: "User Management", icon: "user", url: USERS_URL },
 ];
 
-const adminPages = [ROLES_URL, PROJECT_ASSIGNMENT_URL, USERS_URL];
+const adminPages = [PROJECT_ASSIGNMENT_URL, USERS_URL];
 
 export { navOptions, adminPages };
