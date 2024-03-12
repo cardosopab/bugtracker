a9deb4a610f2cf53025c3c89ece2d35404a35044
Pablo <cardosopab@gmail.com>
2024-03-12 09:43:09 -0500

    Add pagination buttons

---
+++ client/src/views/components/PaginationButtons.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Button, ButtonGroup, Grid } from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+interface PaginationButtonsProps {
+  page: number;
+  totalPages: number;
+  onPageChange: (newPage: number) => void;
+}
+
+const PaginationButtons: React.FC<PaginationButtonsProps> = ({
+  page,
+  totalPages,
+  onPageChange,
+}) => {
+  const handlePageChange = (newPage: number) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage);
+    }
+  };
+
+  return (
+    <Grid container justifyContent="center">
+      <ButtonGroup>
+        <Button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+        >
+          Prev
+        </Button>
+        <Button
+          disabled
+          style={{
+            color: blue[600],
+          }}
+        >
+          {`Page ${page} of ${totalPages}`}
+        </Button>
+        <Button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === totalPages}
+        >
+          Next
+        </Button>
+      </ButtonGroup>
+    </Grid>
+  );
+};
+
+export default PaginationButtons;
