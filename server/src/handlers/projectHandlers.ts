889db993bbeaabd340337f640a21a9147d02ebf4
Pablo <cardosopab@gmail.com>
2024-03-28 22:54:45 -0500

    add personnel by email

--- server/src/handlers/projectHandlers.ts
+++ server/src/handlers/projectHandlers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { validationResult, matchedData } from "express-validator";
 import Project from "../mongoose/schemas/Project";
+import User from "../mongoose/schemas/User";
 
 export const createProjectHandler = async (req: Request, res: Response) => {
   const result = validationResult(req);
@@ -20,6 +21,38 @@ export const createProjectHandler = async (req: Request, res: Response) => {
   } catch (err) {
     console.log(err);
     return res.sendStatus(400);
+  }
+};
+
+export const addPersonnelByEmailHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    console.log(result.array());
+    return res.status(400).send(result.array());
+  }
+
+  const data = matchedData(req);
+  const { projectId, email } = data;
+
+  try {
+    const project = await Project.findById(projectId);
+    if (!project) return res.status(404).send("Project not found");
+
+    const user = await User.findOne({ email: email }).select(
+      "_id, name, email, role"
+    );
+    if (!user) return res.status(404).send("User not found");
+
+    project.personnel.push(user._id);
+    const updatedProject = await project.save();
+
+    return res.status(200).send(user);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
   }
 };
 
@@ -80,6 +113,47 @@ export const readProjectByCompanyHandler = async (
     if (!projects) return res.status(404).send("Projects not found");
 
     return res.status(200).send(projects);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
+
+export const readPaginatedProjectsHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    console.log(result.array());
+    return res.status(400).send(result.array());
+  }
+  const { companyId } = matchedData(req);
+  let { page, pageSize } = matchedData(req);
+
+  try {
+    page = parseInt(page as string) || 1;
+    pageSize = parseInt(pageSize as string) || 10;
+
+    const totalCount = await Project.countDocuments();
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    if (page > totalPages) {
+      return res.status(404).send("Page not found");
+    }
+
+    const skip = (page - 1) * pageSize;
+
+    const projects = await Project.find({ companyId: companyId })
+      .skip(skip)
+      .limit(pageSize);
+
+    console.log("backend", projects.length);
+    return res.status(200).json({
+      projects,
+      page,
+      totalPages,
+    });
   } catch (err) {
     console.log(err);
     return res.sendStatus(500);
