b10e96c084a3b9e6262be4c3c9535bc8bbaf5dc6
Pablo <cardosopab@gmail.com>
2024-03-28 22:46:43 -0500

    Delete extra routes

--- client/src/constants/drawerConstants.ts
+++ client/src/constants/drawerConstants.ts
@@ -1,8 +1,6 @@
 import {
   DASHBOARD_URL,
   KANBAN_URL,
-  PROJECTS_URL,
-  TICKETS_URL,
   PROJECT_ASSIGNMENT_URL,
   USERS_URL,
 } from "./viewEndpoints";
@@ -10,10 +8,6 @@ import {
 const navOptions = [
   { name: "Dashboard", icon: "dashboard", url: DASHBOARD_URL },
   { name: "Kanban Board", icon: "table-chart", url: KANBAN_URL },
-  { name: "Projects Overview", icon: "handy-man", url: PROJECTS_URL },
-  { name: "Tickets Overview", icon: "list", url: TICKETS_URL },
-  { name: "Project Assignment", icon: "group", url: PROJECT_ASSIGNMENT_URL },
-  { name: "User Management", icon: "user", url: USERS_URL },
 ];
 
 const adminPages = [PROJECT_ASSIGNMENT_URL, USERS_URL];
