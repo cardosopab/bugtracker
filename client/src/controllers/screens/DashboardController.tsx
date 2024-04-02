0d22aaafcd1f12913b0a740cf06f23bdfabab47b
Pablo <cardosopab@gmail.com>
2024-04-02 01:03:57 -0500

    Remove logic to form arrays, and receive them from the server.

--- client/src/controllers/screens/DashboardController.tsx
+++ client/src/controllers/screens/DashboardController.tsx
@@ -1,132 +1,88 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../models/redux/store";
-import {
-  priorityOptions,
-  statusOptions,
-  typeOptions,
-} from "../../constants/ticketConstants";
 import { useEffect, useState } from "react";
-import Ticket from "../../models/Ticket";
 import DashboardView from "../../views/screens/dashboard/DashboardView";
-import { useMediaQuery, useTheme } from "@mui/material";
-import { useTicketActions } from "../../models/database/hooks/useTicketActions";
+import { SelectChangeEvent } from "@mui/material";
 import { useProjectActions } from "../../models/database/hooks/useProjectActions";
-import { useUserActions } from "../../models/database/hooks/useUserActions";
+import { useDashboardActions } from "../../models/database/hooks/useDashboardActions";
+import Project from "../../models/Project";
 
 const DashboardController = () => {
-  const tickets = useSelector((state: RootState) => state.tickets.tickets);
-  const projects = useSelector((state: RootState) => state.projects.value);
-  const users = useSelector((state: RootState) => state.users.value);
-  const user = useSelector((state: RootState) => state.auth.currentUser);
-  const companyId = user!.companyId;
-  const { readCompanyTickets } = useTicketActions();
+  const { readProjectData } = useDashboardActions();
   const { readCompanyProjects } = useProjectActions();
-  const { readCompanyUsers } = useUserActions();
-  const [priorityCount, setPriorityCount] = useState<number[]>(
-    new Array(priorityOptions.length).fill(0)
+  const companyId = useSelector(
+    (state: RootState) => state.auth.currentUser!.companyId
   );
-  const [statusCount, setStatusCount] = useState<number[]>(
-    new Array(statusOptions.length).fill(0)
+  const projects = useSelector((state: RootState) => state.projects.value);
+  const [selectedProject, setSelectedProject] = useState<Project | undefined>(
+    undefined
+  );
+  const [priorityCount, setPriorityCount] = useState<number[] | undefined>(
+    undefined
+  );
+  const [statusCount, setStatusCount] = useState<number[] | undefined>(
+    undefined
   );
   const [typeCount, setTypeCount] = useState<
-    { id: number; value: number; label: string }[]
-  >([]);
+    { id: number; value: number; label: string }[] | undefined
+  >(undefined);
   const [personnelCount, setPersonnelCount] = useState<
-    { id: number; value: number; label: string }[]
-  >([]);
-  const theme = useTheme();
-  const isLarge = useMediaQuery(theme.breakpoints.down("lg")); // Check for lg screen
+    { id: number; value: number; label: string }[] | undefined
+  >(undefined);
 
   useEffect(() => {
-    readCompanyTickets(companyId);
     readCompanyProjects(companyId);
-    readCompanyUsers(companyId);
   }, [companyId]);
 
   useEffect(() => {
-    const updatedPriorityCount = new Array(priorityOptions.length).fill(0);
-    const updatedStatusCount = new Array(statusOptions.length).fill(0);
-    const updatedTypeCount: {
-      id: number;
-      value: number;
-      label: string;
-    }[] = [];
-    const updatedPersonnelCount: {
-      id: number;
-      value: number;
-      label: string;
-    }[] = [];
+    if (projects.length > 0 && !selectedProject) {
+      setSelectedProject(projects[0]);
+    }
+  }, [projects, selectedProject]);
 
-    tickets.forEach((ticket: Ticket) => {
-      const priorityIdx = priorityOptions.indexOf(ticket.priority);
-      if (priorityIdx !== -1) {
-        updatedPriorityCount[priorityIdx] += 1;
-      }
-
-      const statusIdx = statusOptions.indexOf(ticket.status);
-      if (statusIdx !== -1) {
-        updatedStatusCount[statusIdx] += 1;
-      }
-
-      const typeIdx = typeOptions.indexOf(ticket.type);
-      if (typeIdx !== -1) {
-        if (!updatedTypeCount[typeIdx]) {
-          updatedTypeCount[typeIdx] = {
-            id: typeIdx,
-            value: 0,
-            label: typeOptions[typeIdx],
-          };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (selectedProject) {
+          const data = await readProjectData(selectedProject!._id);
+          // Update the state variables
+          setPriorityCount(data.priorityCount);
+          setStatusCount(data.statusCount);
+          setTypeCount(data.typeCount);
+          setPersonnelCount(data.topPersonnelCount);
         }
-        updatedTypeCount[typeIdx].value += 1;
+      } catch (error) {
+        console.error("Error fetching project data:", error);
       }
+    };
 
-      const personnel = users.find((user) => user._id === ticket.personnelId);
-      if (
-        personnel &&
-        (ticket.status === statusOptions[1] ||
-          ticket.status === statusOptions[2])
-      ) {
-        const personnelIdx = users.indexOf(personnel);
-        if (personnelIdx !== -1) {
-          if (!updatedPersonnelCount[personnelIdx]) {
-            updatedPersonnelCount[personnelIdx] = {
-              id: personnelIdx,
-              value: 0,
-              label: personnel.name,
-            };
-          }
-          updatedPersonnelCount[personnelIdx].value += 1;
-        }
-      }
-    });
+    if (selectedProject) {
+      fetchData();
+    }
+  }, [selectedProject]);
 
-    let updatedTopTypeCount = [...updatedTypeCount];
-    let updatedTopPersonnelCount = [...updatedPersonnelCount];
+  const handleProjectDropdown = (event: SelectChangeEvent) => {
+    const selectedProjectName = event.target.value as string;
+    const selectedProjectObj = projects.find(
+      (project) => project.name === selectedProjectName
+    );
 
-    // Sort the arrays to keep only the top 4 objects
-    updatedTopTypeCount = updatedTopTypeCount
-      .sort((a, b) => b.value - a.value)
-      .slice(0, 4);
-    updatedTopPersonnelCount = updatedTopPersonnelCount
-      .sort((a, b) => b.value - a.value)
-      .slice(0, 4);
-
-    // Update the state variables
-    setPriorityCount(updatedPriorityCount);
-    setStatusCount(updatedStatusCount);
-    setTypeCount(updatedTopTypeCount);
-    setPersonnelCount(updatedTopPersonnelCount);
-  }, [tickets, users]);
-
+    if (selectedProjectObj) {
+      setSelectedProject(selectedProjectObj);
+    }
+  };
   return (
-    <DashboardView
-      projects={projects}
-      priorityCount={priorityCount}
-      statusCount={statusCount}
-      typeCount={typeCount}
-      personnelCount={personnelCount}
-      isLarge={isLarge}
-    />
+    <>
+      <DashboardView
+        projects={projects}
+        priorityCount={priorityCount}
+        statusCount={statusCount}
+        typeCount={typeCount}
+        personnelCount={personnelCount}
+        selectedProject={selectedProject}
+        handleProjectDropdown={handleProjectDropdown}
+      />
+    </>
   );
 };
 export default DashboardController;
